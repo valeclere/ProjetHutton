2fbcfb8ebe0d1d0bd48b59ce4883b7148a35a662
RemiThln <60218645+RemiThln@users.noreply.github.com>
2020-02-08 11:23:38 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -1824,23 +1824,18 @@ shinyServer(function(input, output,session) {
     content = function(file) {writeRaster(temporaryrast, format="GTiff", file)}
   )
   
-  output$nbGroup <- renderUI(
-    numericInput("nbGroup","Choose the number of group",2,2,length(input$mychooserp)-1,1))
+  output$nbGroup <- renderUI({
+    validate(need(input$methodsmain==4,""))
+    numericInput("nbGroup","Choose the number of group",2,2,length(input$mychooserp)-1,1)})
   
-  output$GroupChoice <- renderUI(
-   # Groups <- list()
-    #for(i in 1:input$nbGroup){
-     # Groups <- list(Groups,i)
-    #}
+  output$GroupChoice <- renderUI({
+    validate(need(input$methodsmain==4,""))
     lapply(input$mychooserp, function(i){
       tmp <- HPN[HPN$Description %in% substr(i,7,700),]$Nickname
       tmpdes <- HPN2[HPN$Description %in% substr(i,7,700)]
       descript <- gsub("_", " ", tmpdes)
       selectInput("Group choice",label=descript,choices=mapply(toString, 1:input$nbGroup),1)
-    }))
-  
-
-  
+    })})
   
   
   
